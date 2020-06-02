182f8f82d6968e9208610f6af1f5f890f6165ce0
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-02 23:49:59 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -24,7 +24,7 @@ class Patient
 
   def doctors
     #binding.pry
-    Appointment.all.collect do |appointment|
+    Doctor.all.collect do |appointment|
       appointment.patient == self
     end
   end
