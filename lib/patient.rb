a13fca7f6c0f7c5229b09453e69998b021aafa59
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-06-02 23:05:45 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+class Patient
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(date, doctor)
+  #  binding.pry
+    Appointment.new(date, self, doctor)
+
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+  def doctors
+    appointments.select do |doctor|
+      doctor.patient == self
+    end
+  end
+
+  def doctors
+
+  end
+
+  def self.all
+    @@all
+  end
+end
